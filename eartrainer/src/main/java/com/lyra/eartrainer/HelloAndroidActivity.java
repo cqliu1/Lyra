fa9882dea165c2f81b36165df6f0f9ef8f67f77c
jeffcirka <jeff123cirka@hotmail.com>
2013-10-30 00:29:57 -0700

    Testing commit and push again

--- eartrainer/src/main/java/com/lyra/eartrainer/HelloAndroidActivity.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/HelloAndroidActivity.java
@@ -17,7 +17,6 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        //This was a test
         TextView text = new TextView(this);
         text.setText("Hello World!");
         setContentView(text);
