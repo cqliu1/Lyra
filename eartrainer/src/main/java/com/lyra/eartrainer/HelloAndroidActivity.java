b8ccbbd720bae247324728f3c925c991be4be8ff
Keilan Jackson <keilan.r.jackson@gmail.com>
2013-11-13 09:21:21 -0700

    Added the options layout

--- eartrainer/src/main/java/com/lyra/eartrainer/HelloAndroidActivity.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/HelloAndroidActivity.java
@@ -17,16 +17,17 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        TextView text = new TextView(this);
-        text.setText("Hello World!");
-        setContentView(text);
+        //TextView text = new TextView(this);
+        //text.setText("Hello World!");
+        //setContentView(text);
+        setContentView(R.layout.options);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.lyra.eartrainer.R.menu.main, menu);
-	return true;
+    	// Inflate the menu; this adds items to the action bar if it is present.
+    	getMenuInflater().inflate(com.lyra.eartrainer.R.menu.main, menu);
+    	return true;
     }
 
 }
