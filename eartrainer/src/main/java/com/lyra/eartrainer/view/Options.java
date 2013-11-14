430811c2de1b37918e87d239c9787db6778ed40b
Johnn Gaio <jj.the.awesome@gmail.com>
2013-11-13 18:55:43 -0700

    pause menus

--- eartrainer/src/main/java/com/lyra/eartrainer/view/Options.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/view/Options.java
@@ -1,16 +1,5 @@
 package com.lyra.eartrainer.view;
 
-import android.R;
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.View;
-
-public class Options extends View {
-
-	public Options(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		View v = findViewById(com.lyra.eartrainer.R.layout.options);
-		
-	}
+public class Options {
 
 }
--- eartrainer/src/main/java/com/lyra/eartrainer/view/Pause.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/view/Pause.java
@@ -1,5 +1,32 @@
 package com.lyra.eartrainer.view;
 
-public class Pause {
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.SeekBar;
 
+import com.lyra.eartrainer.R;
+
+public class Pause {
+	private Button resume;
+	private Button restart;
+	private Button quit;
+	private SeekBar volumeBar;
+	private int volume = 50;
+	
+	public Pause(Activity activity){
+		this.resume = (Button)activity.findViewById(R.id.btnResumeGame);
+		this.restart = (Button)activity.findViewById(R.id.btnRestartGame);
+		this.quit = (Button)activity.findViewById(R.id.btnQuitGame);
+		this.volumeBar = (SeekBar)activity.findViewById(R.id.seekVolume);
+		volumeBar.scrollTo(volume, 1);
+	}
+	public void updateVolume(int newVolume)
+	{
+		volume = newVolume;
+		volumeBar.scrollTo(volume, 1);
+	}
+	
 }
