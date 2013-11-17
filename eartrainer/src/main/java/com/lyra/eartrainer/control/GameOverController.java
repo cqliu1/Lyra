0d636a478f60f39ff39cf1def4140f8aea0692ba
Sean Hanson <smhanso3@asu.edu>
2013-11-17 16:00:35 -0700

    Created Game Over Screen

---
+++ eartrainer/src/main/java/com/lyra/eartrainer/control/GameOverController.java
@@ -0,0 +1,49 @@
+package com.lyra.eartrainer.control;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.lyra.eartrainer.GameOverActivity;
+import com.lyra.eartrainer.R;
+import com.lyra.eartrainer.view.GameOver;
+
+public class GameOverController extends Controller {
+	private GameOver cgoView;
+	 
+	public GameOverController(GameOverActivity gameOverActivity){
+		super(gameOverActivity);
+	}
+	
+	public void initialize(){
+		//creating the view
+		cgoView = new GameOver(activity);
+		//attaching event listeners to view widgets
+		attachEvents();
+	}
+
+	private void attachEvents(){
+		//adding submit button click handler
+		Button btnToReplay = (Button)activity.findViewById(R.id.btnToReplay);
+		btnToReplay.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//fill in later
+			}
+		});
+		Button btnToMainMenu = (Button)activity.findViewById(R.id.btnToMainMenu);
+		btnToMainMenu.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//fill in later
+			}
+		});
+		Button btnToLeaderboardsGameOver = (Button)activity.findViewById(R.id.btnToLeaderboardsGameOver);
+		btnToLeaderboardsGameOver.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				//fill in later
+			}
+		});
+	}
+}
--- eartrainer/src/main/java/com/lyra/eartrainer/view/GameOver.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/view/GameOver.java
@@ -1,5 +1,22 @@
 package com.lyra.eartrainer.view;
 
+import android.app.Activity;
+import android.widget.Button;
+
+import com.lyra.eartrainer.R;
+
 public class GameOver {
+	private Button replay;
+	private Button mainmenu;
+	private Button leaderboard;
+
+	public GameOver(Activity activity) {
+
+		this.replay = (Button) activity.findViewById(R.id.btnToReplay);
+		this.mainmenu = (Button) activity.findViewById(R.id.btnToMainMenu);
+		this.leaderboard = (Button) activity
+				.findViewById(R.id.btnToLeaderboardsGameOver);
+
+	}
 
 }
