de702ac68f760535d4616c5f29b0a9bce2dc2557
jeffcirka <jeff123cirka@hotmail.com>
2014-03-02 19:53:17 -0700

    More

--- eartrainer/src/main/java/com/lyra/eartrainer/dao/LeaderBoardDao.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/dao/LeaderBoardDao.java
@@ -4,11 +4,15 @@ import com.lyra.eartrainer.model.LeaderBoard;
 import com.lyra.eartrainer.model.LeaderBoardEntry;
 
 public interface LeaderBoardDao {
-	
 	// Returns id of added score
 	public int addScore(LeaderBoardEntry scoreEntry) throws DaoParseException;
 	
 	// Returns a Leaderboard with scores from the page
 	public LeaderBoard getScores(int pageNumber) throws DaoParseException;
-	//public LeaderBoardEntry getScore(String identifier) throws DaoParseException;
+
+	// Returns a Leaderboard with scores for the next page
+	public LeaderBoard getNextPage() throws DaoParseException;
+	
+	// Returns a leaderboard with scores for the previous page
+	public LeaderBoard getPrevPage() throws DaoParseException;
 }
