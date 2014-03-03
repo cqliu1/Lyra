67cccd859b83f2fb94bc10824fa318e0961c3a1a
jeffcirka <jeff123cirka@hotmail.com>
2014-03-02 17:44:07 -0700

    More

--- eartrainer/src/main/java/com/lyra/eartrainer/control/LeaderBoardController.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/control/LeaderBoardController.java
@@ -1,13 +1,17 @@
 package com.lyra.eartrainer.control;
 
 import com.lyra.eartrainer.LeaderBoardActivity;
+import com.lyra.eartrainer.view.LeaderboardView;
 
 public class LeaderBoardController extends Controller {
+	private LeaderboardView leaderBoardView;
 	public LeaderBoardController(LeaderBoardActivity leaderBoardActivity){
 		super(leaderBoardActivity);
 	}
 	
 	public void initialize(){
+		//creating the leaderboard view object
+		leaderBoardView = new LeaderboardView();
 		
 	}
 }
---
+++ eartrainer/src/main/java/com/lyra/eartrainer/view/LeaderboardView.java
@@ -0,0 +1,8 @@
+package com.lyra.eartrainer.view;
+
+public class LeaderboardView {
+	public void populateList(){
+		//pulling data from model and displaying it in the view
+		
+	}
+}
