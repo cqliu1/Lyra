121556ed3c4e6efd73a8a4fa4fdb199f19a0868c
jeffcirka <jeff123cirka@hotmail.com>
2014-03-02 18:21:20 -0700

    More

--- eartrainer/src/main/java/com/lyra/eartrainer/control/LeaderBoardController.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/control/LeaderBoardController.java
@@ -1,5 +1,7 @@
 package com.lyra.eartrainer.control;
 
+import android.widget.Toast;
+
 import com.lyra.eartrainer.LeaderBoardActivity;
 import com.lyra.eartrainer.dao.DaoParseException;
 import com.lyra.eartrainer.dao.LeaderBoardDao;
@@ -20,16 +22,37 @@ public class LeaderBoardController extends Controller {
 		activity = leaderBoardActivity;
 		dao = new LeaderBoardDaoImpl();
 		scorePage = 0;
-
 	}
 	
-	public void initialize() throws DaoParseException{
-		
-		LeaderBoard scores = dao.getScores(scorePage);
-		scorePage++;
-		
+	public void initialize(){
 		//creating the leaderboard view object
-		leaderBoardView = new LeaderboardView(activity, scores);
-
+		leaderBoardView = new LeaderboardView(activity);
+		fetchScores();
+	}
+	
+	private void attachEvents(){
+		//TODO add event handlers for buttons
+	}
+	
+	private void fetchScores(){
+		LeaderBoard scores = null;
+		try {
+			scores = dao.getScores(scorePage);
+			scorePage++;
+		} catch(DaoParseException dpe){
+			showError();
+		}
+		
+		if(scores != null){
+			leaderBoardView.populateList(scores);
+		}
+		else {
+			showError();
+		}
+	}
+	
+	private void showError(){
+		//move this into a method in the view
+		Toast.makeText(activity, "There was a problem while displaying the leaderboard results. Please try again later.", Toast.LENGTH_SHORT).show();
 	}
 }
