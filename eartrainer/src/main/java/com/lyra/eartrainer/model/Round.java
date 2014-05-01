69380b614b9e3ad55ac4d1805f7d5529adbdd917
Keilan Jackson <keilan.r.jackson@gmail.com>
2014-04-30 22:31:48 -0700

    fixed the guitar interval bugs

--- eartrainer/src/main/java/com/lyra/eartrainer/model/Round.java
+++ eartrainer/src/main/java/com/lyra/eartrainer/model/Round.java
@@ -29,7 +29,12 @@ public class Round {
 		
 		
 		int min = instrument.getMinNote();
-		int max = instrument.getMaxNote() +1;
+		int max = instrument.getMaxNote()+1;
+		
+		if (GamePlay.instance().getInstrumentType() == InstrumentTypes.GUITAR) {
+			min = getGuitarRealId(min);
+			max = getGuitarRealId(max);
+		}
 		
 		// When we implement intervals, they will be plugged in here
 		if (GamePlay.instance().getMode() == Modes.PRACTICE)
@@ -37,17 +42,6 @@ public class Round {
 			minHalfStep = GamePlay.instance().getLeftInterval();
 			maxHalfStep = GamePlay.instance().getRightInterval();
 		
-			if (instrument2 == InstrumentTypes.GUITAR)
-			{
-				if (minHalfStep > 11)
-					minHalfStep+=2;
-				else if (minHalfStep > 5)
-					minHalfStep+=1;
-				if (maxHalfStep > 11)
-					maxHalfStep+=2;
-				else if (maxHalfStep > 5)
-					maxHalfStep+=1;
-			}
 		}
 		else
 		{
@@ -65,16 +59,9 @@ public class Round {
 			// Make sure the second note is at least minHalfStep away
 			belowMinStep = Math.abs(secondNote-firstNote) < minHalfStep;
 			aboveMaxStep = Math.abs(secondNote-firstNote) > maxHalfStep;
-			if (GamePlay.instance().getHiLo() == HiLo.HiLo && GamePlay.instance().getMode() == Modes.PRACTICE
-					&& firstNote > secondNote)
-				swapNotes();
-			else if (GamePlay.instance().getHiLo() == HiLo.LoHi && GamePlay.instance().getMode() == Modes.PRACTICE
-					&&  secondNote > firstNote)
-				swapNotes();
+
 				
-		} while(belowMinStep || aboveMaxStep || secondNote < 0 || secondNote > (max-min)
-				|| (instrument2 == InstrumentTypes.GUITAR && ((secondNote%5 == 0 || secondNote%5==1)
-						&& (firstNote%5 == 0 || firstNote%5 == 1))));
+		} while(belowMinStep || aboveMaxStep || secondNote < 0 || secondNote > (max-min));
 		
 		finished = false;
 		
@@ -83,63 +70,16 @@ public class Round {
 	//returns true if the player selected the correct result
 	public boolean isCorrect(int note){
 		byte instrument = GamePlay.instance().getInstrumentType();
+	
 		
-		if(note == secondNote){ 
-				return true;
-		} else if(instrument == InstrumentTypes.GUITAR){ //check overlapping notes
-			switch(secondNote) {
-				case 5: //A on E string
-					if(note == 6)
-						return true;
-					break;
-				case 6: //open A
-					if(note == 5)
-						return true;
-					break;
-				case 11: //D on A string
-					if(note == 12)
-						return true;
-					break;
-				case 12: //open D
-					if(note == 11)
-						return true;
-					break;
-				case 17: //G on D string
-					if(note == 18)
-						return true;
-					break;
-				case 18: //open G
-					if(note == 17)
-						return true;
-					break;
-				case 22: //B on G string
-					if(note == 24)
-						return true;
-					break;
-				case 23: //C on G string
-					if(note == 25)
-						return true;
-					break;
-				case 24: //open B
-					if(note == 22)
-						return true;
-					break;
-				case 25: //C on B string
-					if(note == 23)
-						return true;
-					break;	
-				case 29: //E on B string
-					if(note == 30)
-						return true;
-					break;
-				case 30: //open E
-					if(note == 29)
-						return true;
-					break;
-				default:
-						selectionCount++;
-						return false;
-			}
+		if(instrument == InstrumentTypes.GUITAR){
+			int actualNote = getGuitarRealId(note);
+			selectionCount++;
+			return actualNote == secondNote;
+		} else if(instrument == InstrumentTypes.PIANO)
+		{
+			selectionCount++;
+			return note == secondNote;
 		}
 		
 		selectionCount++;
@@ -147,25 +87,80 @@ public class Round {
 	}
 	
 	public int getFirstNote() {
+		// For guitar convert the note id
+		if (GamePlay.instance().getInstrumentType() == InstrumentTypes.GUITAR) {
+			return getGuitarFakeId(firstNote);
+		}
 		return firstNote;
 	}
 
 	public int getSecondNote() {
+		// For guitar, convert the note id
+		if (GamePlay.instance().getInstrumentType() == InstrumentTypes.GUITAR) {
+			return getGuitarFakeId(secondNote);
+		}
 		return secondNote;
 	}
 	
 	public int getInterval() {
 		int returnVal = Math.abs(firstNote-secondNote);
-		if (GamePlay.instance().getInstrumentType() == InstrumentTypes.GUITAR)
-		{
-			if (returnVal > 11)
-				returnVal-=2;
-			else if (returnVal > 5)
-				returnVal--;
-		}
-		//Sean - Add logic to subtract the right amount so that the interval is correct.
 		
 		return returnVal;
+	}
+	
+	// This converts a key'd ID from the UI to the internal id representation for guitar
+	public int getGuitarRealId(int id) {
+		// Fifth open string, fifth fret, offset by 6
+		if(id > 30) {
+			return id-6;
+		} else
+		// Fourth open string on fourth fret, offset by 5
+		if(id > 24) {
+			return id-5;
+		} else
+		// Third open string, offset by 3
+		if(id > 18) {
+			return id-3;
+		} else
+		// Second open string, offset by 2
+		if(id > 12) {
+			return id-2;
+		} else
+		// First open string, so offset by 1
+		if(id > 6) {
+			return id-1;
+		} else
+		// Set the note from musicInfo
+		if(id <= 6) {
+			return id;
+		}
+		return -1;
+	}
+	
+	// This converts the internal representation of ID to UI representation for guitar, accounting for repeated keys
+	public int getGuitarFakeId(int id) {
+		// Fifth open string, fifth fret, offset by 6
+		if(id > 24) {
+			return id+6;
+		} else
+		// Fourth open string on fourth fret, offset by 5
+		if(id > 19) {
+			return id+5;
+		} else
+		// Third open string, offset by 3
+		if(id > 15) {
+			return id+3;
+		} else
+		// Second open string, offset by 2
+		if(id > 10) {
+			return id+2;
+		} else
+		// First open string, so offset by 1
+		if(id > 5) {
+			return id+1;
+		} else {
+			return id;
+		}
 	}
 	
 	public void setFinished(boolean finished) {
